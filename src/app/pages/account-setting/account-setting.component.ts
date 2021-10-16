ff9a21ebd43245d7adb40d4b26b772cc99fe9083
Pablo <Pablofmateo@gmail.com>
2021-10-16 17:10:57 -0300

    Fin seccion 7

---
+++ src/app/pages/account-setting/account-setting.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
+
+@Component({
+  selector: 'app-account-setting',
+  templateUrl: './account-setting.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingComponent implements OnInit {
+
+  constructor( private settingsService: SettingsService ) { }
+
+  ngOnInit(): void {
+    this.settingsService.checkCurrentTheme()
+  }
+
+  changeTheme( theme: string ){
+    this.settingsService.changeTheme( theme );
+
+  }
+
+
+}
