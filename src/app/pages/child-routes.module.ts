3bf107b7c713cae12a2e847150796d1e350fb37b
Pablo <Pablofmateo@gmail.com>
2021-12-12 19:41:22 -0300

    Environment listo

---
+++ src/app/pages/child-routes.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { AccountSettingComponent } from './account-setting/account-setting.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
+
+/// Mantenimientos
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { AdminGuard } from '../guards/admin.guard';
+
+
+const childRoutes:Routes = [
+  { path: '' , component: DashboardComponent , data: { titulo: 'Dashboard' } },
+  { path: 'account-settings' , component: AccountSettingComponent , data: { titulo: 'Ajuste de cuenta' } },
+  { path: 'buscar/:termino' , component: BusquedaComponent , data: { titulo: 'Busquedas' } },
+  { path: 'progress' , component: ProgressComponent , data: { titulo: 'Progress' } },
+  { path: 'grafica1' , component: Grafica1Component  , data: { titulo: 'Grafica1' } },
+  { path: 'promesas' , component: PromesasComponent , data: { titulo: 'Promesas' } },
+  { path: 'rxjs' , component: RxjsComponent , data: { titulo: 'Rxjs' } },
+  { path: 'perfil' , component: PerfilComponent , data: { titulo: 'Perfil de Usuario' } },
+/*   { path: '' , redirectTo: '/dashboard', pathMatch: 'full' }, */
+
+  /// Mantenimientos
+  { path: 'hospitales' , component: HospitalesComponent , data: { titulo: 'Mantenimiento de Hospitales' }  },
+  { path: 'medicos' , component: MedicosComponent , data: { titulo: 'Mantenimiento de Medicos' }  },
+  { path: 'medico/:id' , component: MedicoComponent , data: { titulo: 'Mantenimiento de Medicos' }  },
+
+  /// Rutas de Admin
+  { path: 'usuarios' , canActivate: [ AdminGuard ] , component: UsuariosComponent , data: { titulo: 'Mantenimiento de Usuarios' }  },
+]
+
+
+@NgModule({
+  imports: [
+    CommonModule, RouterModule.forChild( childRoutes )],
+    exports: [ RouterModule]
+
+})
+export class ChildRoutesModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3005/api'
+/*   base_url: 'http://localhost:3005/api' */
+    base_url: 'https://pm-adminpro-backend.herokuapp.com/api'
 };
